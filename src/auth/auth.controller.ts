68a59ea018ba406db9d573de67ef2a334e513b8b
Kajanan <kajanan@intaap.com>
2023-04-04 10:02:30 +0100

    change variable name

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,37 +1,43 @@
-import { Controller, Post, UseGuards, Req, UseFilters , Body , Get , Param } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
-import { AuthService } from './auth.service';
-import { User } from '../user/user.entity';
+import {
+  Controller,
+  Post,
+  UseGuards,
+  Req,
+  UseFilters,
+  Body,
+  Get,
+  Param,
+} from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { Request } from "express";
+import { AuthService } from "./auth.service";
+import { User } from "../user/user.entity";
 
-@Controller('auth')
+@Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
-// ip saving process added by nuwan and kanjanan
-  @UseGuards(AuthGuard('local-sign-up'))
-  @Post('sign-up')
+  constructor(private readonly authService: AuthService) {}
+  // ip saving process added by nuwan and kanjanan
+  @UseGuards(AuthGuard("local-sign-up"))
+  @Post("sign-up")
   async signUp(@Req() req: Request) {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    return this.authService.login(req.user as User,ip);
+    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    return this.authService.login(req.user as User, ip);
   }
-// ip saving process added by nuwan and kanjanan
-  @UseGuards(AuthGuard('local-sign-in'))
-  @Post('sign-in')
+  // ip saving process added by nuwan and kanjanan
+  @UseGuards(AuthGuard("local-sign-in"))
+  @Post("sign-in")
   async login(@Req() req: Request) {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    return this.authService.login(req.user as User,ip);
-    
+    const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+    return this.authService.login(req.user as User, ip);
   }
 
-
-  @Get('check-email/:email')
-  async checkemail(@Param('email') email) {
+  @Get("check-email/:email")
+  async checkemail(@Param("email") email) {
     return this.authService.checkemail(email);
   }
 
-  @Get('account')
+  @Get("account")
   async account(@Req() req: Request) {
-
     const header = req.headers;
 
     return this.authService.account(header.authorization);
@@ -39,11 +45,9 @@ export class AuthController {
 
   @Get()
   async findAll() {
-    const companies =  await this.authService.findAll();
+    const userLoginData = await this.authService.findAll();
     return {
-      companies
+      userLoginData,
     };
   }
-
-
 }
