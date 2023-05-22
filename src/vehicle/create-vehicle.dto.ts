8984d61a99ff7b3bde49ef0c02d0f18e1ce0d3c0
Nuwan <nuwan@intaap.com>
2023-05-22 12:25:04 +0530

    dto of vehicle update

--- src/vehicle/create-vehicle.dto.ts
+++ src/vehicle/create-vehicle.dto.ts
@@ -1,6 +1,7 @@
 import { TripEntity } from "src/trip/trip.entity";
 import { VehicleTypeEntity } from "src/vehicle-type/vehicle-type.entity";
 import { AccidentUpload } from "src/accident-upload/accident-upload.entity";
+import { DefectTrip } from "src/defect-trip/defect-trip.entity";
 export class CreateVehicleDto {
     id: number;
     vehicleName: string;
@@ -11,5 +12,6 @@ export class CreateVehicleDto {
     vehicletype: VehicleTypeEntity;
     vehicleaccident:AccidentUpload[];
     odometer: string;
+    vehicledefecttrip:DefectTrip[]
 
 }
