63571c5495f4350491873d3aec2ff8c7b1d21713
Nuwan <nuwan@intaap.com>
2023-05-31 14:57:54 +0530

    mail template

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -447,12 +447,12 @@ export class CompaniesService {
 
       if (data.existingCompanyEmail) {
         if (data.users[0].password && data.users[0].email) {
-          await this.mailservice.sendcompanyCreate(data.users[0].password, "user", data.existingCompanyEmail, data.users[0].email);
+          await this.mailservice.sendcompanyCreate(data.users[0].password, "user", data.users[0].email, data.users[0].email);
         }
         else if (data.users[0].email) {
-          await this.mailservice.sendcompanyCreate("password is not change", "user", data.existingCompanyEmail, data.users[0].email);
+          await this.mailservice.sendcompanyCreate("password is not change", "user", data.users[0].email, data.users[0].email);
         } else if (data.users[0].password) {
-          await this.mailservice.sendcompanyCreate(data.users[0].password, "user", data.existingCompanyEmail, "your username is not change");
+          // await this.mailservice.sendcompanyCreate(data.users[0].password, "user", data.existingCompanyEmail, "your username is not change");
         } else {
           await this.mailservice.sendcompanyCreate("password is not change. But user profile details are change", "user", data.existingCompanyEmail, "your username is not change");
         }
