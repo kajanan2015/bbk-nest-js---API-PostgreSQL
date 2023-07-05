332d1216ac45c3f2042041a2cfb5832f3860df89
Nuwan <nuwan@intaap.com>
2023-07-05 11:25:31 +0530

    work pattern code generate

--- src/company-work-pattern/company-work-pattern.module.ts
+++ src/company-work-pattern/company-work-pattern.module.ts
@@ -5,9 +5,11 @@ import { CompanyWorkPattern } from './company-work-pattern.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompaniesEntity } from 'src/companies/companies.entity';
 import { User } from 'src/user/user.entity';
+import { SystemCodeService } from 'src/system-code/system-code.service';
+import { SystemCode } from 'src/system-code/system-code.entity';
 @Module({
-  imports:[TypeOrmModule.forFeature([CompanyWorkPattern,User])],
+  imports:[TypeOrmModule.forFeature([CompanyWorkPattern,User,SystemCode])],
   controllers: [CompanyWorkPatternController],
-  providers: [CompanyWorkPatternService]
+  providers: [CompanyWorkPatternService,SystemCodeService]
 })
 export class CompanyWorkPatternModule {}
--- src/company-work-pattern/company-work-pattern.service.ts
+++ src/company-work-pattern/company-work-pattern.service.ts
@@ -4,20 +4,29 @@ import { UpdateCompanyWorkPatternDto } from './update-company-work-pattern.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CompanyWorkPattern } from './company-work-pattern.entity';
 import { Repository } from 'typeorm';
+import { SystemCodeService } from 'src/system-code/system-code.service';
 @Injectable()
 export class CompanyWorkPatternService {
   constructor(
     @InjectRepository(CompanyWorkPattern)
-    private readonly patternrepository:Repository<CompanyWorkPattern>
+    private readonly patternrepository:Repository<CompanyWorkPattern>,
+    private readonly systemcodeService: SystemCodeService,
   ){}
  async create(createCompanyWorkPatternDto: CreateCompanyWorkPatternDto) {
   await this.findbypattername(createCompanyWorkPatternDto.workPatternName,createCompanyWorkPatternDto.company)
+  const responsesystemcode = await this.systemcodeService.findOne("workpattern");
+  const systemcode = responsesystemcode.code + "" + responsesystemcode.startValue;
+  const newstartvalue = {
+    startValue: responsesystemcode.startValue + 1,
+  };
+  createCompanyWorkPatternDto.workPatternCode=systemcode;
   createCompanyWorkPatternDto.workType=0;
   const response= await this.patternrepository.create(createCompanyWorkPatternDto)
   const withputtimepattern=await this.patternrepository.save(response);
   createCompanyWorkPatternDto.workType=1;
   const response2= await this.patternrepository.create(createCompanyWorkPatternDto)
   const withtimepattern=await this.patternrepository.save(response2)
+  await this.systemcodeService.update(responsesystemcode.id, newstartvalue);
 
   if(withputtimepattern&&withtimepattern){
     return "successfully created"
@@ -49,7 +58,13 @@ async findbypattername(name:string,companyId){
   }
 
  async update(id: number, updateCompanyWorkPatternDto: UpdateCompanyWorkPatternDto) {
-   return await this.patternrepository.update(id,updateCompanyWorkPatternDto)
+  const data= await this.patternrepository.findOne(id)
+  const response=  await this.patternrepository.find({where:{workPatternCode:data.workPatternCode}})
+  for(const data in response){
+   console.log(data['id'],90909090990)
+    await this.patternrepository.update(data['id'],updateCompanyWorkPatternDto);
+  }
+  return "updated success"
   }
 
   remove(id: number) {
