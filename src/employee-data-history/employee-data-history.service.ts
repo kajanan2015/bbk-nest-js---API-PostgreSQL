bd18ca1e91373d60617c8be8e66e0e8112f5ceb7
Supun <supun@intaap.com>
2023-08-03 16:41:58 +0530

    findAllEmpDataHistory api

--- src/employee-data-history/employee-data-history.service.ts
+++ src/employee-data-history/employee-data-history.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateEmployeeDataHistoryDto } from './update-employee-data-history.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, getManager } from 'typeorm';
 import { EmployeeDataHistory } from './employee-data-history.entity';
 import { EmployeeInfo } from 'src/employee-module/employee-module.entity';
 
@@ -66,29 +66,186 @@ export class EmployeeDataHistoryService {
   }
 
   async findAllEmpDataHistory(createEmployeeDataHistoryDto) {
-    const [results, totalCount] = await this.empDataHistoryRepository.findAndCount({
+
+    const [res, totalCount] = await this.empDataHistoryRepository.findAndCount({
       where: {
         employeeId: createEmployeeDataHistoryDto.employeeId,
       },
       relations: ['updated_by', 'created_by'],
-      order: {
-        start_date: 'DESC',
-      },
+      // order: {
+      //   start_date: 'DESC',
+      // },
       // skip: createEmployeeDataHistoryDto.page * createEmployeeDataHistoryDto.pageSize,
       // take: createEmployeeDataHistoryDto.pageSize,
     });
 
-    const historyData = results.filter(function (row) {
+    var groupBy = function (xs, key) {
+      return xs.reduce(function (rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+      }, {});
+    };
+
+    const results = res.filter(function (row) {
       return Math.floor(new Date(row.start_date).getTime() / 86400000) <= Math.floor(new Date().getTime() / 86400000)
     })
 
-    console.log(historyData, 22222222222222222222)
+    const historyData = groupBy(results, 'type');
 
-    return {
-      historyList: historyData,
-      totalCount,
-      totalPages: Math.ceil(totalCount / createEmployeeDataHistoryDto.pageSize),
-    };
+    const difference = (obj1, obj2) => {
+      const result = {};
+      if (Object.is(obj1, obj2)) {
+        return undefined;
+      }
+      if (!obj2 || typeof obj2 !== 'object') {
+        return obj2;
+      }
+      Object.keys(obj1 || {}).concat(Object.keys(obj2 || {})).forEach(key => {
+        if (key == 'created_at' || key == 'updated_at' || key == 'updated_by' || key == 'id') {
+        } else if (key == 'created_by') {
+          result[key] = obj1[key];
+        } else if (key == 'empProvidedCopy' || key == 'visaDoc' || key == 'officialDoc' || key == 'refdoc' || key == 'drivingLicenceDoc' || key == 'tachoDoc' || key == 'cpcCardDoc') {
+          const value = difference(obj1?.[key]?.[0]?.['docPath'], obj2?.[key]?.[0]?.['docPath']);
+          if (value !== undefined) {
+            result[key] = obj1[key];
+          }
+        } else {
+          if (obj2[key] !== obj1[key] && !Object.is(obj1[key], obj2[key])) {
+            result[key] = [obj2[key]];
+          }
+          if (typeof obj2[key] === 'object' && typeof obj1[key] === 'object') {
+            const value = difference(obj1[key], obj2[key]);
+            if (value !== undefined) {
+              result[key] = value;
+            }
+          }
+        }
+      });
+      return result;
+    }
+
+    let result = [];
+
+    const entityManager = getManager();
+
+    const empTypesList = await entityManager.query(
+      `SELECT * FROM employee_type`
+    );
+
+    const genderList = await entityManager.query(
+      `SELECT * FROM gender`
+    );
+
+    const maritalStatusList = await entityManager.query(
+      `SELECT * FROM marital_status`
+    );
+
+    const designationList = await entityManager.query(
+      `SELECT * FROM employee_designation`
+    );
+
+    const countryList = await entityManager.query(
+      `SELECT * FROM country`
+    );
+
+    const bankList = await entityManager.query(
+      `SELECT * FROM bank`
+    );
+
+    const visaTypeList = await entityManager.query(
+      `SELECT * FROM visa_type`
+    );
+
+    Object.keys(historyData).forEach(function (key, index){
+      const data = historyData[key];
+      const tableData = [];
+      for (let row in data){
+        const rowData = data[row]
+        const jsonRow = JSON.parse(rowData.data);
+        if(jsonRow.hasOwnProperty('employeeType')){
+          let employeeType = empTypesList.find(type => type.id == jsonRow.employeeType);
+          jsonRow.employeeType = employeeType
+        }
+        if(jsonRow.hasOwnProperty('gender')){
+          let gender = genderList.find(gender => gender.id == jsonRow.gender);
+          jsonRow.gender = gender
+        }
+        if(jsonRow.hasOwnProperty('maritalStatus')){
+          let maritalStatus = maritalStatusList.find(status => status.id == jsonRow.maritalStatus);
+          jsonRow.maritalStatus = maritalStatus
+        }
+        if(jsonRow.hasOwnProperty('designation')){
+          let designation = designationList.find(designation => designation.id == jsonRow.designation);
+          jsonRow.designation = designation
+        }
+        if(jsonRow.hasOwnProperty('addressCountry')){
+          let addressCountry = countryList.find(country => country.id == jsonRow.addressCountry);
+          jsonRow.addressCountry = addressCountry
+        }
+        if(jsonRow.hasOwnProperty('bank')){
+          let bankName = bankList.find(bank => bank.id == jsonRow.bankName);
+          jsonRow.bankName = bankName
+        }
+        if(jsonRow.hasOwnProperty('visaType')){
+          let visaType = visaTypeList.find(visaType => visaType.id == jsonRow.visaType);
+          jsonRow.visaType = visaType
+        }
+        tableData.push({
+            id: rowData?.id,
+            start_date: rowData?.start_date.toString(),
+            updated_at: rowData?.updated_at,
+            created_at: rowData?.created_at,
+            updated_by: rowData?.updated_by?.firstName,
+            created_by: rowData?.created_by?.firstName,
+            ...jsonRow
+        })        
+    }
+
+      for (let row in tableData?.reverse()) {
+        if (data[Number(row) - 1]) {
+          const json1 = tableData[row];
+          const json2 = tableData[Number(row) - 1];
+          const res = difference(json1, json2)
+          result.push(res)
+        }
+      }
+
+    });
+
+
+    // const query: SelectQueryBuilder<EmployeeDataHistory> = getConnection()
+    //   .getRepository(EmployeeDataHistory)
+    //   .createQueryBuilder("employeeHistory")
+    //   .leftJoinAndSelect("employeeHistory.created_by", "created_by")
+    //   .leftJoinAndSelect("employeeHistory.updated_by", "updated_by")
+    //   .leftJoinAndSelect("employeeHistory.employeeId", "employee")
+    //   .andWhere("employeeHistory.employeeId = :id", { id: +createEmployeeDataHistoryDto.employeeId })
+    //   // .andWhere("employeeHistory.start_date <= :date", { date: start_date })
+    //   // .groupBy('employeeHistory.type')
+    //   // .orderBy('employeeHistory.start_date', 'DESC')
+
+    //   // .addGroupBy('employeeHistory.type');
+
+    //   const historyData = await query.getMany();
+    //   const tableData = [];
+    //   for (let row in historyData) {
+    //     const rowData = historyData[row]
+    //     const jsonRow = JSON.parse(rowData.data);
+    //     // let gender = genderList.find(gender => gender.id == jsonRow.gender);
+    //     // let maritalStatus = maritalStatusList.find(status => status.id == jsonRow.maritalStatus);
+    //     // jsonRow.gender = gender
+    //     // jsonRow.maritalStatus = maritalStatus
+    //     tableData.push({
+    //         id: rowData?.id,
+    //         start_date: rowData?.start_date,
+    //         updated_at: rowData?.updated_at,
+    //         created_at: rowData?.created_at,
+    //         updated_by: rowData?.updated_by?.firstName,
+    //         created_by: rowData?.created_by?.firstName,
+    //         ...jsonRow
+    //     })
+    // }
+    return result
   }
 
   async findEmpDataShedule(createEmployeeDataHistoryDto) {
