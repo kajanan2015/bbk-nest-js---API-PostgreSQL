5b2b55767809459e68a5161bd682dd9badfa5570
Nuwan <nuwan@intaap.com>
2023-08-17 16:21:48 +0530

    work pattern assign

--- src/company-work-pattern/company-work-pattern.service.ts
+++ src/company-work-pattern/company-work-pattern.service.ts
@@ -6,6 +6,8 @@ import { CompanyWorkPattern } from './company-work-pattern.entity';
 import { Repository } from 'typeorm';
 import { SystemCodeService } from 'src/system-code/system-code.service';
 import { EmployeeDataHistory } from 'src/employee-data-history/employee-data-history.entity';
+import { WorkType } from './company-work-pattern.entity';
+import { WorkPatternType } from './company-work-pattern.entity';
 @Injectable()
 export class CompanyWorkPatternService {
   constructor(
@@ -15,7 +17,7 @@ export class CompanyWorkPatternService {
     @InjectRepository(EmployeeDataHistory)
     private readonly employedatahistory: Repository<EmployeeDataHistory>,
   ) { }
-  async create(createCompanyWorkPatternDto: CreateCompanyWorkPatternDto) {
+  async create(createCompanyWorkPatternDto) {
     await this.findbypattername(createCompanyWorkPatternDto.workPatternName, createCompanyWorkPatternDto.company)
     const responsesystemcode = await this.systemcodeService.findOne("workpattern");
     const systemcode = responsesystemcode.code + "" + responsesystemcode.startValue;
@@ -23,7 +25,7 @@ export class CompanyWorkPatternService {
       startValue: responsesystemcode.startValue + 1,
     };
     createCompanyWorkPatternDto.workPatternCode = systemcode;
-    createCompanyWorkPatternDto.workType = 0;
+    createCompanyWorkPatternDto.workType = WorkType.WITHOUTTIME;
     const response = await this.patternrepository.create(createCompanyWorkPatternDto)
     const withputtimepattern = await this.patternrepository.save(response);
 
@@ -31,7 +33,7 @@ export class CompanyWorkPatternService {
     //const responsehistorywithputtime = await this.employedatahistory.create({ type:"work-pattern-history", data: JSON.stringify(response), company:createCompanyWorkPatternDto.company,createdBy:createCompanyWorkPatternDto.patterncreate,workpattern:withputtimepattern});
 
     //const reswithputtime = await this.employedatahistory.save(responsehistorywithputtime);
-    createCompanyWorkPatternDto.workType = 1;
+    createCompanyWorkPatternDto.workType = WorkType.WITHTIME;
     const response2 = await this.patternrepository.create(createCompanyWorkPatternDto)
     const withtimepattern = await this.patternrepository.save(response2)
     await this.systemcodeService.update(responsesystemcode.id, newstartvalue);
@@ -45,7 +47,8 @@ export class CompanyWorkPatternService {
   }
 
   async findAll() {
-    return await this.patternrepository.find({ relations: ['company', 'patterncreate'] })
+    const workpatterndata= await this.patternrepository.find({ relations: ['company', 'patterncreate'] })
+    return workpatterndata; 
   }
 
   async findbypattername(name: string, companyId) {
@@ -57,13 +60,35 @@ export class CompanyWorkPatternService {
 
   async findOne(id: number) {
     const workpattern = await this.patternrepository.findOne(id);
+    let patternType;
+    if(workpattern.patternType==WorkPatternType.WEEK){
+      patternType=1;
+    }else if(workpattern.patternType==WorkPatternType.MOREWEEK){
+      patternType=2;
+    }else if(workpattern.patternType==WorkPatternType.CUSTOM){
+      patternType=3;
+    }else{
+
+    }
+    let workType;
+    if(workpattern.workType==WorkType.WITHOUTTIME){
+      workType=0;
+    }else if(workpattern.workType==WorkType.WITHTIME){
+      workType=1;
+    }else{
+    }
+    delete workpattern.patternType;
+    workpattern.patternType=patternType;
+    delete workpattern.workType;
+    workpattern.workType=workType;
+
     if (!workpattern) {
       throw new NotFoundException(` ID '${id}' not found`);
     }
     return workpattern;
   }
 
-  async update(id: number, updateCompanyWorkPatternDto: UpdateCompanyWorkPatternDto) {
+  async update(id: number, updateCompanyWorkPatternDto) {
     const data = await this.patternrepository.findOne(id)
 
     const response = await this.patternrepository.find({ where: { workPatternCode: data.workPatternCode } })
@@ -98,5 +123,9 @@ export class CompanyWorkPatternService {
 
   remove(id: number) {
     return `This action removes a #${id} companyWorkPattern`;
+  }
+
+  async assignworkpatternemployee(){
+
   }
 }
