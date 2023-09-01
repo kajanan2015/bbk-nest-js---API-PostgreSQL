e78fd158ee357ffa8ed51f588dfe04874f0d3364
Supun <supun@intaap.com>
2023-09-01 12:17:56 +0530

    findCurrentWorkPattern api

--- src/company-work-pattern/company-work-pattern.service.ts
+++ src/company-work-pattern/company-work-pattern.service.ts
@@ -3,7 +3,7 @@ import { CreateCompanyWorkPatternDto } from './create-company-work-pattern.dto';
 import { UpdateCompanyWorkPatternDto } from './update-company-work-pattern.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CompanyWorkPattern } from './company-work-pattern.entity';
-import { Repository, SelectQueryBuilder, getConnection } from 'typeorm';
+import { LessThanOrEqual, Repository, SelectQueryBuilder, getConnection } from 'typeorm';
 import { SystemCodeService } from 'src/system-code/system-code.service';
 import { EmployeeDataHistory } from 'src/employee-data-history/employee-data-history.entity';
 import { WorkType } from './company-work-pattern.entity';
@@ -96,21 +96,39 @@ export class CompanyWorkPatternService {
     return workpattern;
   }
 
+  // ** Fine employee current work pattern
   async findCurrentWorkPattern(empId) {
+    // ** current date
     const date = new Date();
-    const workpattern = await this.employeeassignrepo.find({ where: { employeeId: empId, status: WorkPatternStatus.ACTIVE } });
-    // const workPatternInfo = await this.employeeassigninforepo.find({ where: { workpatternId: workpattern['id'] }});
+    // const workpattern = await this.employeeassignrepo.find({ where: { employeeId: empId, assign_at: LessThanOrEqual(date), status: WorkPatternStatus.ACTIVE }, order:{ assign_at: 'DESC' } });
+    
+    // ** get current work pattern
     const query: SelectQueryBuilder<EmployeeAssignWorkPattern> = getConnection()
     .getRepository(EmployeeAssignWorkPattern)
     .createQueryBuilder("workpattern")
+    .leftJoinAndSelect("workpattern.workpatternId", "workpatternId")
     .andWhere("workpattern.employeeId = :empId", { empId })
     .andWhere("workpattern.assign_at <= :date", { date })
     .andWhere("workpattern.status = :status", { status: WorkPatternStatus.ACTIVE })
     .orderBy("workpattern.assign_at", 'DESC')
-
-    const data = await query.getMany();
-    return data[0];
     
+    // ** Current work pattern
+    const workPattern = await query.getOne();
+
+    // ** Get current work pattern info
+    const workpatternInfo = await this.employeeassigninforepo.findOne({ where: { assignpatternId: workPattern?.['assign_id'], assign_at: format(
+      date,
+      'yyyy-MM-dd'
+    ) }});
+    // ** Current pattern rounds
+    const patternRounds = await this.employeeassigninforepo.find({ where: { assignpatternId: workPattern?.['assign_id'], pattern_round: workpatternInfo?.['pattern_round'] }});
+
+    return { 
+      patternInfo: patternRounds,
+      workPattern: workPattern?.['workpatternId'],
+      startedDate: workPattern?.['assign_at']
+    };
+
   }
 
   async findOne(id: number) {
